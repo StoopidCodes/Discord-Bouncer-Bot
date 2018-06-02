70ef3a1801b11d4d06357c3647ea09e9c57b0944
StoopidCodes <39773112+StoopidCodes@users.noreply.github.com>
2018-06-02 18:05:51 +0400

    Add files via upload

---
+++ BouncerBot/Modules/Ping.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncerBot.Modules
+{
+    public class Ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("hello")]
+        public async Task PingAsync()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.WithTitle("Welcome,")
+                .WithDescription("Make sure you've read the rules.")
+                .WithColor(Color.Red);
+
+            await ReplyAsync("", false, builder.Build());
+        }
+
+        [Command("hello again!")]
+        public async Task PingAsync2()
+        {
+            await ReplyAsync("Welcome back, come on in.");
+        }
+
+        [Command("fight")]
+        public async Task PingAsync3(string name)
+        {
+            await ReplyAsync($"{name} is no match for me.");
+        }
+
+        [Command("lets fight")]
+        public async Task PingAsync4()
+        {
+            await ReplyAsync("You *really* don't want to do that.");
+        }
+
+        [Command("say")]
+        public async Task PingAsync5([Remainder]string stuffToSay)
+        {
+            await ReplyAsync(stuffToSay);
+        }
+
+        [Command("bhelp")]
+        public async Task PingAsync6()
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.AddField("Overview:", "List of Bouncer's commands and prefix(es):")
+                .AddInlineField("Prefix(es):", "'.'")
+                .AddInlineField("Commands:", "hello    hello again!    fight (username)    lets fight     say (whatever you want bouncer to say)")
+                .WithColor(Color.Gold);
+            
+            await ReplyAsync("", false, builder.Build());
+        }
+    }
+}
---
+++ BouncerBot/Modules/timestamps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BouncerBot.Modules
+{
+    class timestamps
+    {
+        //Notes for timestamps and video of origin
+        //20:00 - stuffToSay - https://www.youtube.com/watch?v=x_OIDHxK1Ag
+        //9:16 - addField - https://www.youtube.com/watch?v=x_OIDHxK1Ag
+        //21:30 - commandPermission - https://www.youtube.com/watch?v=x_OIDHxK1Ag
+    }
+}
